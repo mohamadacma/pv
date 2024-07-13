0b8dac4cad866e80b42462313fb8b3093e946856
Mohamad <mohamadacma@gmail.com>
2024-07-13 10:33:27 -0500

    help

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,6 @@ function initEssentials() {
     initTypewriter();
     initHorrorTypewriter();
 }
-javascriptCopyimport { initTypewriter } from './typewriter.js';
-import { loadPhotoGallery } from './gallery.js';
-import { initHackerEffect } from './hackerEffect.js';
-import { initModalHandler } from './modalHandler.js';
-import { initSmoothScrolling } from './smoothScroll.js';
-import { showGrid } from './gridHandler.js';
-
-function initEssentials() {
-    initSmoothScrolling();
-    initTypewriter();
-    initHorrorTypewriter();
-}
 
 function initHorrorTypewriter() {
     const container = document.querySelector('.horror-typewriter-text');
