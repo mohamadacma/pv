4a2907ddf448ff6b827e219f9422e9d47e9d2d66
Mohamad <mohamadacma@gmail.com>
2024-07-13 10:30:21 -0500

    added horor effect

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,75 @@ import { showGrid } from './gridHandler.js';
 function initEssentials() {
     initSmoothScrolling();
     initTypewriter();
+    initHorrorTypewriter();
 }
+javascriptCopyimport { initTypewriter } from './typewriter.js';
+import { loadPhotoGallery } from './gallery.js';
+import { initHackerEffect } from './hackerEffect.js';
+import { initModalHandler } from './modalHandler.js';
+import { initSmoothScrolling } from './smoothScroll.js';
+import { showGrid } from './gridHandler.js';
+
+function initEssentials() {
+    initSmoothScrolling();
+    initTypewriter();
+    initHorrorTypewriter();
+}
+
+function initHorrorTypewriter() {
+    const container = document.querySelector('.horror-typewriter-text');
+    const interests = [
+        "Technology",
+        "Philosophy",
+        "Science",
+        "Art",
+        "Music"
+    ];
+    let currentIndex = 0;
+
+    function typeNextInterest() {
+        if (currentIndex < interests.length) {
+            typeHorrorStyle(interests[currentIndex], container, () => {
+                setTimeout(() => {
+                    eraseText(container, () => {
+                        currentIndex++;
+                        typeNextInterest();
+                    });
+                }, 1000);
+            });
+        }
+    }
+
+    function typeHorrorStyle(text, element, callback) {
+        let index = 0;
+        const interval = setInterval(() => {
+            if (index < text.length) {
+                element.textContent += text[index];
+                playTypeSound();
+                index++;
+            } else {
+                clearInterval(interval);
+                if (callback) callback();
+            }
+        }, 100);
+    }
+
+    function eraseText(element, callback) {
+        const interval = setInterval(() => {
+            if (element.textContent.length > 0) {
+                element.textContent = element.textContent.slice(0, -1);
+                playEraseSound();
+            } else {
+                clearInterval(interval);
+                if (callback) callback();
+            }
+        }, 50);
+    }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     initTypewriter();
+    initHorrorTypewriter();
     loadPhotoGallery();
     initHackerEffect();
     initModalHandler();
