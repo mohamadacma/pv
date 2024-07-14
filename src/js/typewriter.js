1a22ef1991d9bb1fa7358c02805660200cb8e784
Mohamad <mohamadacma@gmail.com>
2024-07-14 16:01:56 -0500

    made things dramatic like in the trailers

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -1,18 +1,20 @@
 export function initTypewriter() {
-    const text = `Hello! I'm M!\n and this is my cafe!`;
-    const typingElement = document.querySelector('.typewriter-text');
+    const text = `Hello! I'm M!\n and this is my cafe! I am interested in`;
+    const typingElement = document.querySelector('.tarantino-typewriter-text');
     typingElement.textContent = '';
     let i = 0;
 
-    function typeWriter() {
-        if (i < text.length) {
-            typingElement.textContent += text.charAt(i);
-            i++;
-            setTimeout(typeWriter, 50);
-        } else {
-            typingElement.style.borderRight = 'none';
-        }
-    }
+   function typeWriter() {
+           if (i < text.length) {
+               if (text.charAt(i) === '\n') {
+                   typingElement.innerHTML += '<br>';
+               } else {
+                   typingElement.textContent += text.charAt(i);
+               }
+               i++;
+               setTimeout(typeWriter, 100);
+           }
+       }
 
     typeWriter();
 }
