e575db32575688d128a1b94a2e8a4b2dbbee9631
Mohamad <mohamadacma@gmail.com>
2024-11-06 16:42:13 -0600

    amusing ourselves

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,6 +1,25 @@
 // posts.js
 const blogPosts = [
     {
+        id: 2,
+        title: "Amusing Ourselves to Death",
+        date: "2024-11-06",
+        time: "12:00",
+        preview: "If we go back to the 17th and 18th century, the majority of the discourse at the time is expository, rational, long-form and sequential...",
+        content: `If we go back to the 17th and 18th century, the majority of the discourse at the time is expository, rational, long-form and sequential. People had a typographic mind; the only medium of communication available was the printed word, so people had to read and think and engage actively with the medium which, in turns, made them more thoughtful, critical, and inclined to engage in the expositional form of discourse. People wouldnt have minded listeinng in to a debate for 7 hours. Debates and discussions were serious at the time.
+
+As we can see the medium, is not merely a message, the medium is the metaphor, the metaphor for something else, for shaping a culture, for changing behaviours. It is akin to a clock, weve invented a clock as a metaphore for controlling infinity, making fun of it; similarly, a sunglass that aids in seeing, was a metaphor for rebellion against nature: we dont need to succumb to nature, we can change it. Oratory discourse aka the rise of the typographic mind thanks to the printing press marked the apogee of seriousness and critical thought. the ratio of information to action was super high, the "news" were relevant, the truth was reachable through long-form discourse, everything you hear was contained in a context familiar to your model of the world.
+
+Then came the telegraph, an instanenouse transfer of information over space. The information suddenly becomes irrelevant, the problems of the world suddnely becomes your problem, the decontextualization arrived, the relevance started to fade in the back of a sea of irrelevance. The medium of the telegraph was encroaching on the clearness of a typographic mind. Fast forward and the TV arrives and thats when things start to go haywire and the typographic mind dwindle. The promise of the TV was to turn everything into entertainment. Religion, Politics, News, Education. Suddenly everything had to conform to what the medium of TV required: make things that are appealing to a wide audience, short-form and entertaining, and switch between widely different topics using the Now....then method of TV. Irrelevane had suddnely become and ocean, and context were erased. The goal was to keep you watching.
+
+Orwell was scared of a world where books are banned, information is restricted, and truth is concealed, but turns out the world we ended up in is a Huxlyan word: no need to ban books, restrict information, or censoring the truth. Just make the truth hard to get to; drown it in a sea of irrelevance and make things entertaining.
+
+You can extend Neil Postman worries to the 21st century, and think of the medium of nowadays, TikTok, X, Instagram... How are these mediums shaping the culture of the day? What are they a metaphor for? I think It is clear that we're stil living in that huxlyan world of scrolling ourselves to death and entertaining ourselves with the illusion of knowledge with what it is merely sound-bites of decontextualised sea of irrelevance that is disguising as relevant and worthy.
+
+This book has reminded me to revive my typographical mind and seek long-form writing because the habits of mind that these mediums are imposing on us are ones that encourage rage, delusions, and atrophy of our ciritical faculties. DO you think X can ever me a medium where the next Illiad will be produced? or like Instagram the next education frontier? the medium is the metaphor!`,
+        tags: ["philosophy", "history", "media", "society", "technology", "culture"]
+    },
+    {
         id: 1,
         title: "4-day trip to New Orleans: Four Days is Too Much",
         date: "2024-10-20",
@@ -36,7 +55,7 @@ I did a lot of walking, it looked like an aged city. While I was walking around 
 
 <img src="images/NOLAEight.jpeg" alt="Street Scene" class="post-image" />
 <span class="image-caption">got to the other side of the mississipi river by Ferry to try a taco place, worth it </span>`,
-        tags: ["travel", "new-orleans", "museums", "jazz"]
+        tags: ["travel"]
     }
 ];
 
